5ea8df58120272e67924af7fb8d4800582a0b0d0
Diego Castelo <diegoreboucascastelo@gmail.com>
2026-06-08 21:26:44 -0300

    assembler agr recebe arqv direito e asm corrigido

---
+++ problemas/assembler.py
@@ -0,0 +1,168 @@
+import sys
+
+PROLOGUE_SIZE = 4
+# Diego
+
+fsrc = open(str(sys.argv[1]), 'r')
+
+instruction_set = {
+    'ld'   :  0x01, 
+    'add'  :  0x02, 
+    'sub'  :  0x0D, 
+    'goto' :  0x09, 
+    'mov'  :  0x06,  
+    'jz'   :  0x0B, 
+    'halt' :  0xFF,
+    'jn'   :  0x11, 
+}
+lines = []       # armazena as linhas limpas
+labels = {}      # tabela de Símbolos ('nome': endereco_byte)
+binary_data = bytearray() 
+
+def calculate_instruction_size(inst, ops):
+    if (inst == 'halt') or (inst == 'wb'):
+        return 1
+    elif (inst in ['ld', 'add', 'sub', 'mov', 'jz', 'goto', 'jn']):
+        return 2
+    elif (inst == 'ww'):
+        return 4
+    else:
+        return 0
+
+def _find_first_code_label():
+    for line in lines:
+        if (line[0] not in instruction_set) and (line[0] not in ('wb', 'ww')):
+            if (len(line) > 1) and (line[1] not in ('wb', 'ww')):
+                return line[0]
+        elif line[0] in instruction_set:
+            return None
+    return None
+
+
+def first_look():
+    actual_adress = PROLOGUE_SIZE # já existe um byte 0 padrão na posição 0
+
+    for line in lines:
+        if (line[0] in instruction_set) or line[0] in ['wb', 'ww']:
+            inst = line[0]
+            ops = line[1:]
+            actual_adress += calculate_instruction_size(inst, ops)
+        else:
+            label_name = line[0]
+            labels[label_name] = actual_adress
+
+            if (len(line) > 1):
+                inst = line[1]
+                ops = line[2:]
+                actual_adress += calculate_instruction_size(inst, ops)
+
+
+def encode_math(inst, ops):
+    generated_bytes = []
+
+    if len(ops) > 1:
+        if ops[0] == 'x':
+            var_name = ops[1]
+        elif ops[1] == 'x':
+            var_name = ops[0]
+        else:
+            return generated_bytes
+
+        if var_name in labels:
+            byte_adress = labels[var_name]
+            divided_adress = byte_adress // 4
+            
+            generated_bytes.append(instruction_set[inst])
+            generated_bytes.append(divided_adress)
+    return generated_bytes
+
+def encode_jump(inst, ops):
+    generated_bytes = []
+
+    if (len(ops) > 0):
+        label_name = ops[0]
+        if (label_name in labels):
+            generated_bytes.append(instruction_set[inst])
+            generated_bytes.append(labels[label_name])
+    return generated_bytes
+
+def encode_ww(inst, ops):
+        generated_bytes = []
+        if (len(ops)) > 0:
+            try:
+                val = int(ops[0])
+                
+                generated_bytes.append(val & 0xFF)
+                generated_bytes.append((val & 0xFF00) >> 8)
+                generated_bytes.append((val & 0xFF0000) >> 16)
+                generated_bytes.append((val & 0xFF000000) >> 24)
+            except ValueError:
+                pass 
+        return generated_bytes
+
+def second_look(destination_path):
+    routes = {
+        'ld'   : encode_math,
+        'add'  : encode_math,
+        'sub'  : encode_math,
+        'mov'  : encode_math,
+        'goto' : encode_jump,
+        'jz'   : encode_jump,
+        'jn'   : encode_jump,
+        'ww'   : encode_ww
+    }
+
+    first_code = _find_first_code_label()
+    if (first_code is not None):
+        code_addr = labels[first_code]
+        binary_data.append(instruction_set['goto'])
+        binary_data.append(code_addr)
+        binary_data.append(0x00)
+        binary_data.append(0x00)
+    else:
+        for k in labels:
+            labels[k] -= PROLOGUE_SIZE
+    
+    for tokens in lines:
+        if (tokens[0] in instruction_set) or (tokens[0] in ['wb', 'ww']):
+            inst = tokens[0]
+            ops = tokens[1:]
+        else:
+            if (len(tokens) > 1):
+                inst = tokens[1]
+                ops = tokens[2:]
+            else:
+                continue
+        
+        if (inst == 'halt'):
+            binary_data.append(instruction_set['halt'])
+        elif (inst in routes):
+            instruction_bytes = routes[inst](inst, ops)
+            if (not instruction_bytes):
+                print(f"erro de sintaxe na instrução {inst} {ops}")
+                return False
+            binary_data.extend(instruction_bytes)
+        else:
+            print(f'o assembler não soube oq fazer aqq: {inst}')
+            return False
+    
+    try:
+        with open(destination_path, 'wb') as fdst:
+            fdst.write(binary_data)
+        return True
+    except Exception as e:
+        print(f'erro ao gravar arquivo final: {e}')
+        return False
+
+def run(destination_path):        
+    first_look()
+    second_look(destination_path)
+
+
+for line in fsrc:
+    tokens = line.strip().lower().replace(',', '').split()
+    if tokens:
+        lines.append(tokens)
+
+run(str(sys.argv[2]))
+fsrc.close()
--- problemas/prob2.asm
+++ problemas/prob2.asm
@@ -11,116 +11,100 @@ one        ww 1
 two        ww 2
 
 
-inicio     
-           ld x, entrada1
-           jz not_prime   
-           sub x, one
-           jz not_prime    
+inicio              ld x, entrada1
+                     jz not_prime   
+                     sub x, one
+                     jz not_prime    
  
-           ld x, two
-           mov D, x
+                      ld x, two
+                      mov D, x
 
-check_prime
-           ld x, entrada1
-           sub x, D
-           jz is_prime     
+check_prime         ld x, entrada1
+                    sub x, D
+                    jz is_prime     
 
-           ld x, entrada1
-           mov temp, x
+                    ld x, entrada1
+                    mov temp, x
 
 mod_loop_1
-           ld x, temp
-           sub x, D
-           jz not_prime    
-           jn next_d_1
-           mov temp, x
-           goto mod_loop_1
+                    ld x, temp
+                    sub x, D
+                    jz not_prime    
+                    jn next_d_1
+                    mov temp, x
+                    goto mod_loop_1
 
-next_d_1
-           ld x, D
-           add x, one
-           mov D, x
-           goto check_prime
+next_d_1            ld x, D
+                    add x, one
+                    mov D, x
+                    goto check_prime
 
-is_prime
-           ld x, entrada1
-           add x, one
-           mov cand, x    
-           ld x, zero
-           mov Sum, x
-           ld x, one
-           mov D, x
+is_prime            ld x, entrada1
+                    add x, one
+                    mov cand, x    
+                    ld x, zero
+                    mov Sum, x
+                    ld x, one
+                    mov D, x
 
-sum_divs_loop
-           ld x, cand
-           sub x, D
-           jz end_prog     
+sum_divs_loop       ld x, cand
+                    sub x, D
+                    jz end_prog     
 
-           ld x, cand
-           mov temp, x
+                    ld x, cand
+                    mov temp, x
 
-mod_loop_2
-           ld x, temp
-           sub x, D
-           jz is_divisor
-           jn not_divisor
-           mov temp, x
-           goto mod_loop_2
+mod_loop_2          ld x, temp
+                    sub x, D
+                    jz is_divisor
+                    jn not_divisor
+                    mov temp, x
+                     goto mod_loop_2
 
-is_divisor
-           ld x, Sum
-           add x, D
-           mov Sum, x
+is_divisor          ld x, Sum
+                    add x, D
+                    mov Sum, x
 
-not_divisor
-           ld x, D
-           add x, one
-           mov D, x
-           goto sum_divs_loop
+not_divisor         ld x, D
+                    add x, one
+                    mov D, x
+                    goto sum_divs_loop
 
 
-not_prime
-           ld x, entrada1
-           add x, one
-           mov cand, x     
+not_prime           ld x, entrada1
+                    add x, one
+                    mov cand, x     
 
-check_cand
-           ld x, two
-           mov D, x
+check_cand          ld x, two
+                    mov D, x
 
-test_cand_prime
-           ld x, cand
-           sub x, D
-           jz found_next_prime ;
+test_cand_prime     ld x, cand
+                    sub x, D
+                    jz found_next_prime
 
-           ld x, cand
-           mov temp, x
+                    ld x, cand
+                    mov temp, x
 
-mod_loop_3
-           ld x, temp
-           sub x, D
-           jz cand_failed      
-           jn cand_next_d
-           mov temp, x
-           goto mod_loop_3
+mod_loop_3          ld x, temp
+                    sub x, D
+                    jz cand_failed      
+                    jn cand_next_d
+                    mov temp, x
+                    goto mod_loop_3
 
-cand_next_d
-           ld x, D
-           add x, one
-           mov D, x
-           goto test_cand_prime
+cand_next_d         ld x, D
+                    add x, one
+                    mov D, x
+                    goto test_cand_prime
 
-cand_failed
-           ld x, cand
-           add x, one
-           mov cand, x         
-           goto check_cand
+cand_failed         ld x, cand
+                    add x, one
+                    mov cand, x         
+                    goto check_cand
 
-found_next_prime
-           ld x, cand
-           mov Sum, x
+found_next_prime    ld x, cand
+                    mov Sum, x
 
-end_prog
-           ld x, Sum
-           mov saida, x
-           halt
+end_prog            ld x, Sum
+                    mov saida, x
+                    halt
